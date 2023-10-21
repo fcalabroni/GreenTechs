cb0e3af22ddde64261400f0264393ac8b7bbed43
Lorenzo Rinaldi <lorenzo.rinaldi13@gmail.com>
2023-10-21 16:03:35 +0200

    Update 6. PBT & EROI.py

--- 6. PBT & EROI.py
+++ 6. PBT & EROI.py
@@ -10,7 +10,7 @@ paths = 'Paths.xlsx'
 
 years = range(2011,2020)
 
-satellite_account = 'Energy Carrier Supply - Total'
+satellite_accounts = ['GHGs','Energy Carrier Supply - Total']
 price_logics = ['Constant']
 region = 'EU27+UK'
 
@@ -41,139 +41,146 @@ conv_factors = {
     }
 
 
-
-#%% read and aggregate footprints
-f = pd.read_csv(
-    f"{pd.read_excel(paths, index_col=[0]).loc['Results',user]}\\Footprints - Physical units\\{satellite_account}.csv",
-    index_col=[0,1,2,3,4],
-    )
-f['Scenario'] = [i.split(' - ')[0] for i in f.index.get_level_values('Scenario')]
-f = f.query("Scenario==@price_logics")
-f = f.drop('Scenario',axis=1)
-
-f = f.groupby(['Activity to','Scenario']).sum()
-f['Scenario'] = [i.split(' - ')[0] for i in f.index.get_level_values('Scenario')]
-f['Year'] = [i.split(' - ')[1] for i in f.index.get_level_values('Scenario')]
-f['Performance'] = [i.split(' - ')[2] for i in f.index.get_level_values('Scenario')]
-
-scenarios = sorted(list(set(f.index.get_level_values('Scenario'))))
-f = f.droplevel('Scenario')
-f.reset_index(inplace=True)
-
-#%% read shocked tables
-world = {}
-for scenario in scenarios:
-    if scenario.split(' - ')[0] in price_logics:
-        world[scenario] = mario.parse_from_txt(f"{pd.read_excel(paths, index_col=[0]).loc['Database',user]}\\d. Shock - Endogenization of capital\\{scenario}\\coefficients", table='SUT', mode="coefficients")
-
-#%% Numerator
-IN = {}
-
-for tech in caps:
-    IN[tech] = pd.DataFrame()
-    for scenario in scenarios:
-        
-        s = scenario.split(' - ')[0]
-        y = scenario.split(' - ')[1]
-        p = scenario.split(' - ')[2]
-        
-        value = f.query(f"`Activity to`=='{tech}' & Scenario==@s & Year==@y & Performance==@p")
-        value.set_index(['Activity to','Scenario','Year','Performance'],inplace=True)
-        
-        IN[tech] = pd.concat([IN[tech],value],axis=0)
-       
-#%% Denominator
-OUT = pd.DataFrame()
-for scenario in scenarios:
+#%%
+for satellite_account in satellite_accounts:
     
-    s = scenario.split(' - ')[0]
-    y = scenario.split(' - ')[1]
-    p = scenario.split(' - ')[2]
-        
-    prod = world[scenario].X.loc[(region,sN,ee),'production']
-    prod /= prod.sum().sum()
-    prod = prod.to_frame()
+    print(f"\n{satellite_account}")
     
-    prod.index = prod.index.get_level_values(2)
-    
-    value = f.query("`Activity to`==@ee & Scenario==@s & Year==@y & Performance==@p")
-    value.set_index(['Activity to','Scenario','Year','Performance'],inplace=True)
-    value.index = value.index.get_level_values(0)
-    
-    prod.index.names = value.index.names
-    
-    OUT = pd.concat([
-        OUT,
-        pd.DataFrame(
-            np.multiply(value,prod).sum().sum()*conv_factors[satellite_account],
-            index = pd.MultiIndex.from_arrays([[s],[y],[p]], names=['Scenario','Year','Performance']),
-            columns = ['Value'],
-            )],
-        axis=0,
+    print("   read and aggregate footprints")
+    f = pd.read_csv(
+        f"{pd.read_excel(paths, index_col=[0]).loc['Results',user]}\\Footprints - Physical units\\{satellite_account}.csv",
+        index_col=[0,1,2,3,4],
         )
-   
-#%% Payback-time      
-from copy import deepcopy as dc
-PBT = dc(IN)
-PBT_merged = pd.DataFrame()
-
-for tech in caps:
-    for scenario in scenarios:
+    f['Scenario'] = [i.split(' - ')[0] for i in f.index.get_level_values('Scenario')]
+    f = f.query("Scenario==@price_logics")
+    f = f.drop('Scenario',axis=1)
     
+    f = f.groupby(['Activity to','Scenario']).sum()
+    f['Scenario'] = [i.split(' - ')[0] for i in f.index.get_level_values('Scenario')]
+    f['Year'] = [i.split(' - ')[1] for i in f.index.get_level_values('Scenario')]
+    f['Performance'] = [i.split(' - ')[2] for i in f.index.get_level_values('Scenario')]
+    
+    scenarios = sorted(list(set(f.index.get_level_values('Scenario'))))
+    f = f.droplevel('Scenario')
+    f.reset_index(inplace=True)
+
+    print("   parse tables")
+    world = {}
+    for scenario in scenarios:
+        if scenario.split(' - ')[0] in price_logics:
+            world[scenario] = mario.parse_from_txt(f"{pd.read_excel(paths, index_col=[0]).loc['Database',user]}\\d. Shock - Endogenization of capital\\{scenario}\\coefficients", table='SUT', mode="coefficients")
+
+    print("   calc numerator")
+    IN = {}
+    
+    for tech in caps:
+        IN[tech] = pd.DataFrame()
+        for scenario in scenarios:
+            
+            s = scenario.split(' - ')[0]
+            y = scenario.split(' - ')[1]
+            p = scenario.split(' - ')[2]
+            
+            value = f.query(f"`Activity to`=='{tech}' & Scenario==@s & Year==@y & Performance==@p")
+            value.set_index(['Activity to','Scenario','Year','Performance'],inplace=True)
+            
+            IN[tech] = pd.concat([IN[tech],value],axis=0)
+       
+    print("   calc denominator")
+    OUT = pd.DataFrame()
+    for scenario in scenarios:
+        
         s = scenario.split(' - ')[0]
         y = scenario.split(' - ')[1]
         p = scenario.split(' - ')[2]
-    
-        PBT[tech].loc[(tech,s,y,p),'Value'] /= OUT.loc[(s,y,p),'Value']
+            
+        prod = world[scenario].X.loc[(region,sN,ee),'production']
+        prod /= prod.sum().sum()
+        prod = prod.to_frame()
         
-    PBT[tech] /= 8760/12  # in months
-    PBT_merged = pd.concat([PBT_merged,PBT[tech]],axis=0)
+        prod.index = prod.index.get_level_values(2)
+        
+        value = f.query("`Activity to`==@ee & Scenario==@s & Year==@y & Performance==@p")
+        value.set_index(['Activity to','Scenario','Year','Performance'],inplace=True)
+        value.index = value.index.get_level_values(0)
+        
+        prod.index.names = value.index.names
+        
+        OUT = pd.concat([
+            OUT,
+            pd.DataFrame(
+                np.multiply(value,prod).sum().sum()*conv_factors[satellite_account],
+                index = pd.MultiIndex.from_arrays([[s],[y],[p]], names=['Scenario','Year','Performance']),
+                columns = ['Value'],
+                )],
+            axis=0,
+            )
+   
+    print("   calc pbt")
+    from copy import deepcopy as dc
+    PBT = dc(IN)
+    PBT_merged = pd.DataFrame()
+    
+    for tech in caps:
+        for scenario in scenarios:
+        
+            s = scenario.split(' - ')[0]
+            y = scenario.split(' - ')[1]
+            p = scenario.split(' - ')[2]
+        
+            PBT[tech].loc[(tech,s,y,p),'Value'] /= OUT.loc[(s,y,p),'Value']
+            
+        PBT[tech] /= 8760/12  # in months
+        PBT_merged = pd.concat([PBT_merged,PBT[tech]],axis=0)
 
 
-#%% Export
-for tech in caps:
-    PBT[tech].to_csv(f"{pd.read_excel(paths, index_col=[0]).loc['Results',user]}\\Payback-time\\{satellite_account}\\{tech}.csv")
-PBT_merged.to_csv(f"{pd.read_excel(paths, index_col=[0]).loc['Results',user]}\\Payback-time\\{satellite_account}\\All.csv")
+    print("   export csvs")
+    for tech in caps:
+        PBT[tech].to_csv(f"{pd.read_excel(paths, index_col=[0]).loc['Results',user]}\\Payback-time\\{satellite_account}\\{tech}.csv")
+    PBT_merged.to_csv(f"{pd.read_excel(paths, index_col=[0]).loc['Results',user]}\\Payback-time\\{satellite_account}\\All.csv")
 
-#%% Plot
-color_map = {
-    '<b>PV': '#ffc24b',
-    '<b>Onshore wind': '#219ebc',
-    '<b>Offshore wind': '#219ebc', 
-    }
+    print("   plot")
+    color_map = {
+        '<b>PV': '#ffc24b',
+        '<b>Onshore wind': '#219ebc',
+        '<b>Offshore wind': '#219ebc', 
+        }
+    
+    to_plot = PBT_merged.reset_index()
+    for tech,name in caps.items():
+        to_plot = to_plot.replace(tech,f"<b>{name}")
+    
+    fig = px.box(
+        to_plot,
+        x = 'Activity to',
+        y = 'Value',
+        color = 'Activity to',
+        color_discrete_map = color_map,
+        points='all'
+        )
+    
+    if satellite_account == 'GHGs':
+        title = '<b>GHG payback time of PV, onshore and offshoree wind technology capacities <br>Exiobase v3.8.2 2011-2019, refined with MARIO'
+    if satellite_account == 'Energy Carrier Supply - Total':
+        title = '<b>Energy payback time of PV, onshore and offshoree wind technology capacities <br>Exiobase v3.8.2 2011-2019, refined with MARIO'
+    
+    fig.update_layout(
+        title = title,
+        showlegend=False,
+        yaxis_range=[0,max(to_plot['Value']*1.2)],
+        yaxis_title='Months',
+        xaxis_title=None,
+        template = 'plotly_white',
+        font_family = 'HelveticaNeue Light',
+        )
+    
+    if satellite_account == 'GHGs':
+        fig.write_html('Plots/GHGPBT.html', auto_open=True)
+    if satellite_account == 'Energy Carrier Supply - Total':
+        fig.write_html('Plots/EPBT.html', auto_open=True)
 
-to_plot = PBT_merged.reset_index()
-for tech,name in caps.items():
-    to_plot = to_plot.replace(tech,f"<b>{name}")
 
-fig = px.box(
-    to_plot,
-    x = 'Activity to',
-    y = 'Value',
-    color = 'Activity to',
-    color_discrete_map = color_map,
-    points='all'
-    )
-
-if satellite_account == 'GHGs':
-    title = '<b>GHG payback time of PV, onshore and offshoree wind technology capacities <br>Exiobase v3.8.2 2011-2019, refined with MARIO'
-if satellite_account == 'Energy Carrier Supply - Total':
-    title = '<b>Energy payback time of PV, onshore and offshoree wind technology capacities <br>Exiobase v3.8.2 2011-2019, refined with MARIO'
-
-fig.update_layout(
-    title = title,
-    showlegend=False,
-    yaxis_range=[0,max(to_plot['Value']*1.2)],
-    yaxis_title='Months',
-    xaxis_title=None,
-    template = 'plotly_white',
-    font_family = 'HelveticaNeue Light',
-    )
-
-if satellite_account == 'GHGs':
-    fig.write_html('Plots/GHGPBT.html', auto_open=True)
-if satellite_account == 'Energy Carrier Supply - Total':
-    fig.write_html('Plots/EPBT.html', auto_open=True)
+#%%
 
     
 
