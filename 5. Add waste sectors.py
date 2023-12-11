205c11c007bbc2c9e535a2458706ba5b827f79db
fcalabroni <102296141+fcalabroni@users.noreply.github.com>
2023-12-11 15:06:35 +0100

    Update 5.

--- 5. Add waste sectors.py
+++ 5. Add waste sectors.py
@@ -43,12 +43,12 @@ waste_sectors= [
 
 #%% Getting excel templates to add waste sectors
 path_waste_sector = f"{pd.read_excel(paths, index_col=[0]).loc['Add Sectors',user]}\\new_waste_sector.xlsx"
-world[year].get_add_sectors_excel(new_sectors = waste_sectors, regions= [world[year].get_index('Region')[0]],path=path_waste_sector, item='Sectors')
+# world[year].get_add_sectors_excel(new_sectors = waste_sectors, regions= [world[year].get_index('Region')[1]],path=path_waste_sector, item='Sector')
 
 #%% Adding new commodities and activities
 
 for year in years:
-    world[year].add_sectors(io=path_waste_sector, new_sectors= waste_sectors, regions= [world[year].get_index('Region')[0]], item='Sectors', inplace=True)
+    world[year].add_sectors(io=path_waste_sector, new_sectors= waste_sectors, regions= [world[year].get_index('Region')[1]], item='Sectors', inplace=True)
     
 #%%  Aggregated database with new sectors to txt
 
