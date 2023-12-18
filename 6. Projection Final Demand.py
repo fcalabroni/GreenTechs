f432545bde1e31ca1343cf591f693bcbf377b2fc
fcalabroni <102296141+fcalabroni@users.noreply.github.com>
2023-12-18 12:51:09 +0100

    Completed 6.

--- 6. Projection Final Demand.py
+++ 6. Projection Final Demand.py
@@ -8,7 +8,7 @@ import mario
 import pandas as pd
 import numpy as np
 
-user = "MBV"
+user = "CF"
 sN = slice(None)
 history = range(2000,2021)
 years = range(2021,2101)
@@ -26,7 +26,7 @@ for year in history:
 #%% Getting excel templates to aggregate raw Exiobase
 path_aggr_IOT  = r"Aggregations\Aggregation_raw_IOT.xlsx"
 # exio_iot[year].get_aggregation_excel(path_aggr_IOT)
-
+path_GDP =
 #%% Aggregating exiobase as other models
 for year in history:
     exio_iot[year].aggregate(path_aggr_IOT, levels=["Region"])
@@ -36,23 +36,46 @@ for year in history:
 #extrapolate in a dataframe the FD for each year
 FD = {}
 for year in history:
-    FD[year] = exio_iot[year].Y.sum(axis=1)
-    #FD[year] = exio_iot[year].Y.groupby(level=[ 0],sort=False, axis=1).sum()
+    #FD[year] = exio_iot[year].Y.sum(axis=1)
+    FD[year] = exio_iot[year].Y.groupby(level=[ 0],sort=False, axis=1).sum()
     
 #%% Projection FD
 # Calculating average growth from historic data for each sector
 Rn = FD[2020]/FD[2000]
-rn = np.power(Rn.values, 1/20)
+Rn = Rn.fillna(1)
+Rn.replace([np.inf, -np.inf], 1, inplace=True)
 
-rate= {}
+rn = np.power(Rn.values, 1/20)
+rn = pd.DataFrame(rn, index=Rn.index, columns = Rn.columns)
+
+#%%
+
 FD_elastic = {}
+sn = {}
+fileGDP = f"{pd.read_excel(paths, index_col=[0]).loc['GDP projection',user]}"
+GDP_rate = pd.read_excel(fileGDP,'GDP rate',header=0,index_col=0)
 for i in years:
-    FD_elastic[i]= {}
-    rate[i]= rn
-FD_elastic[0] = FD[2020]*rn
+    FD_elastic[i] = 0
+    sn[i] = 0
+    
+FD_elastic[2021] = FD[2020] * rn
 for i in years:
-    FD_elastic[i+1] = FD_elastic[i] * rate[i]
-    #sn[i] = 
+    FD_elastic[i+1] = FD_elastic[i] * rn
+    sn[i] = FD_elastic[i]/FD_elastic[i].sum(axis=0)
+    
+FD_GDP = {}
+FD_proj = {}
+
+for i in years:
+    FD_GDP[i] = 0
+    FD_proj[i] = 0
+    
+FD_GDP[2021] = FD[2020].sum(axis=0)*(1+GDP_rate.loc[:,2021])
+FD_proj[2021] = FD_GDP[2021]*sn[2021]
+
+for i in range(2022,2101):
+    FD_GDP[i] = FD_GDP[i-1].groupby(level=[ 0],sort=False, axis=0).sum()*(1+GDP_rate.loc[:,i])
+    FD_proj[i] = FD_GDP[i]*sn[i]
     
     
 #%% Building the FD useful for the Database 
